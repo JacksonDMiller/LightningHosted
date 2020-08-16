79889fad8baa737ce7a7f06339f7fb35dff97b23
Jackson Miller <jackson.sbe@gmail.com>
2020-08-16 18:54:39 +0900

    in process of debuging the lock centering function

--- src/client/Components/ImageCard.js
+++ src/client/Components/ImageCard.js
@@ -1,205 +1,273 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom'
-import { store } from '../Context/Store';
-import { viewportContext } from '../Context/GetWindowDimensions'
-
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { store } from "../Context/Store";
+import { viewportContext } from "../Context/GetWindowDimensions";
 
 export default function ImageCard(props) {
-    const globalState = useContext(store);
-    const { dispatch } = globalState;
+  const globalState = useContext(store);
+  const { dispatch } = globalState;
 
-    const { imageId,
-        fileType,
-        fileName,
-        caption,
-        title,
-        views,
-        numberOfComments,
-        orientation,
-        paymentRequired,
-        payStatus
-    } = props.imageData
+  const {
+    imageId,
+    fileType,
+    fileName,
+    caption,
+    title,
+    views,
+    numberOfComments,
+    orientation,
+    paymentRequired,
+    payStatus,
+    height,
+  } = props.imageData;
 
-    useEffect(() => {
-        var elems = document.querySelectorAll('.tooltipped');
-        var instances = M.Tooltip.init(elems,);
-        return () => {
-            if (instances.length) {
-                instances.forEach(instance => instance.destroy());
-            }
-        }
-    }, [])
+  useEffect(() => {
+    var elems = document.querySelectorAll(".tooltipped");
+    var instances = M.Tooltip.init(elems);
+    return () => {
+      if (instances.length) {
+        instances.forEach((instance) => instance.destroy());
+      }
+    };
+  }, []);
 
+  const [upvotes, setUpovotes] = useState(props.imageData.upvotes);
 
+  const { width } = useContext(viewportContext);
+  const [upvoted, setUpvoted] = useState(() => {
+    if (
+      globalState.state.upvoted &&
+      globalState.state.upvoted.includes(imageId)
+    ) {
+      return "upvoted";
+    } else {
+      return;
+    }
+  });
+  const { profile, share } = props;
+  const upvote = async (imageId) => {
+    if (globalState.state.auth === false) {
+      M.toast({ html: "<a href='/login'>Please Login First</a>" });
+    } else {
+      if (!globalState.state.upvoted.includes(imageId)) {
+        setUpovotes(upvotes + 1);
+        setUpvoted("upvoted");
+      } else {
+        setUpovotes(upvotes - 1);
+        setUpvoted("");
+      }
+      const res = fetch("/api/upvote/" + imageId);
+      const data = await (await res).json();
+      dispatch({ type: "UPDATE", payload: data.user });
+    }
+  };
 
-    const [upvotes, setUpovotes] = useState(props.imageData.upvotes)
-
-    const { width } = useContext(viewportContext);
-    const [upvoted, setUpvoted] = useState(() => {
-        if (globalState.state.upvoted && globalState.state.upvoted.includes(imageId)) {
-            return 'upvoted';
-        }
-        else { return }
-    })
-
-
-
-    const { profile, share } = props
-    const upvote = async (imageId) => {
-        if (globalState.state.auth === false) {
-            M.toast({ html: "<a href='/login'>Please Login First</a>" })
-        }
-        else {
-            if (!globalState.state.upvoted.includes(imageId)) {
-                setUpovotes(upvotes + 1)
-                setUpvoted('upvoted')
-            }
-            else {
-                setUpovotes(upvotes - 1)
-                setUpvoted('')
-            }
-            const res = fetch('/api/upvote/' + imageId);
-            const data = await (await res).json();
-            dispatch({ type: 'UPDATE', payload: data.user });
-        }
+  const media = () => {
+    let lock = "";
+    if (paymentRequired && profile && payStatus === false) {
+      let top = ((height / 2 - 42) / height) * 100;
+      let nWidth =
+        ((props.imageData.width / 2 + 42) / props.imageData.width) * 100;
+        console.log(height)
+      console.log((height / 2 + 42));
+      lock = (
+        <div
+          className="lock tooltipped"
+          data-position="bottom"
+          data-tooltip="This image requires a payment click to learn more"
+          style={{ top: top + "%", left: nWidth + "%" }}
+        >
+            <i className="material-icons large">lock</i>
+        </div>
+      );
     }
 
-    const media = () => {
-        let lock = ''
-        if (paymentRequired && profile && payStatus === false) {
-            lock = <div
-                className="lock valign-wrapper tooltipped"
-                data-position="bottom"
-                data-tooltip="This image requires a payment click to learn more">
-                <span className='center-align center'>
-                    <i className="material-icons large">lock</i>
-                </span>
-            </div>
-        }
-
-
-        if (fileType === 'mp4') {
-            if (share) {
-                return <span><video autoPlay muted loop className="responsive-mp4">
-                    <source src={"/api/i/" + fileName} type="video/mp4" />
-                </video>
-                    {lock}
-                </span>
-            }
-            else {
-                return <span><video disableremoteplayback={'true'} autoPlay muted loop className="responsive-mp4">
-                    <source src={"/api/i/" + fileName} type="video/mp4" />
-                </video>
-                    {lock}
-                </span>
-            }
-        }
-        if (fileType === 'gif') {
-            return <span>< img src={"/api/i/" + fileName} alt="image" ></img>
-                {lock}
-            </span>
-        }
-        if (share) {
-            return <span><img className='img-payment-required' src={"/api/i/" + fileName} alt="image"></img>
-                {lock}
-            </span>
-        }
-        return <span><img className='img-payment-required' src={"/api/t/" + fileName} alt="image"></img>
+    if (fileType === "mp4") {
+      if (share) {
+        return (
+          <span>
+            <video autoPlay muted loop className="responsive-mp4">
+              <source src={"/api/i/" + fileName} type="video/mp4" />
+            </video>
             {lock}
+          </span>
+        );
+      } else {
+        return (
+          <span>
+            <video
+              disableremoteplayback={"true"}
+              autoPlay
+              muted
+              loop
+              className="responsive-mp4"
+            >
+              <source src={"/api/i/" + fileName} type="video/mp4" />
+            </video>
+            {lock}
+          </span>
+        );
+      }
+    }
+    if (fileType === "gif") {
+      return (
+        <span>
+          <img src={"/api/i/" + fileName} alt="image"></img>
+          {lock}
         </span>
+      );
     }
-
-    const containerClasses = () => {
-        if (share) {
-            return "card center " + orientation + '-own'
-        }
-        if (width < 500) {
-            return 'card'
-        }
-        else { return 'card card-hover' }
+    if (share) {
+      return (
+        <span>
+          <img
+            className="img-payment-required"
+            src={"/api/i/" + fileName}
+            alt="image"
+          ></img>
+          {lock}
+        </span>
+      );
     }
-
     return (
-        <div className={containerClasses()}>
-            {title ? <div>
-                <p className="flow-text black-text caption-text">{title}</p>
-            </div> : null}
-            {share ?
-                <div>
-                    <div className="card-image">
+      <span>
+        <img
+          className="img-payment-required"
+          src={"/api/t/" + fileName}
+          alt="image"
+        ></img>
+        {lock}
+      </span>
+    );
+  };
 
-                        {media()}
-                    </div>
-                    {caption ? <div>
-                        <p className="flow-text black-text caption-text">{caption}</p>
-                    </div> : null}
-                </div> :
-                <Link to={'/s/' + imageId}>
-                    <div className="card-image">
+  const containerClasses = () => {
+    if (share) {
+      return "card center " + orientation + "-own";
+    }
+    if (width < 500) {
+      return "card";
+    } else {
+      return "card card-hover";
+    }
+  };
 
-                        {media()}
-                    </div>
-                    {caption ?
-                        <div>
-                            <p className="flow-text black-text caption-text">{caption.length > 60 ? caption.substring(0, 60) + '...' : caption}</p>
-                        </div> : null}
-                </Link>}
+  return (
+    <div className={containerClasses()}>
+      {share ? (
+        <div>
+          {title ? (
+            <span className="card-title word-break">{title}</span>
+          ) : null}
+          <div className="card-image">{media()}</div>
+          {caption ? (
+            <div>
+              <p className="flow-text black-text card-content caption-text">
+                {caption}
+              </p>
+            </div>
+          ) : null}
+        </div>
+      ) : (
+        <Link to={"/s/" + imageId}>
+          {title ? (
+            <span className="card-title word-break">{title}</span>
+          ) : null}
+          <div className="card-image">{media()}</div>
+          {caption ? (
+            <div>
+              <p className="flow-text black-text card-content caption-text">
+                {caption.length > 60
+                  ? caption.substring(0, 60) + "..."
+                  : caption}
+              </p>
+            </div>
+          ) : null}
+        </Link>
+      )}
 
-            {profile ?
-                <div className="card-content row profile-image-stats">
-                    <span className="center-align col s3 ">
-                        {globalState.state.upvoted.includes(imageId) ? < i onClick={(e) => upvote(imageId, e)}
-                            className="material-icons image-icon upvoted upvote">keyboard_arrow_up</i> :
-                            < i onClick={() => upvote(imageId)}
-                                className="material-icons image-icon upvote">keyboard_arrow_up</i>
-                        }
-                        {upvotes}
-                    </span>
-                    <Link to={'/s/' + imageId}>
-                        <span className="center-align col s3 ">
-                            <i className="material-icons image-icon">remove_red_eye</i> {views}
-                        </span>
-                    </Link>
-                    <Link to={'/s/' + imageId}>
-                        <span className="center-align col s3 ">
-                            <i className="material-icons image-icon">comment</i> {numberOfComments}
-                        </span>
-                    </Link>
-                    <a className="modal-trigger" href="#deleteModal" onClick={() => { props.deleteImage(imageId) }}>
-                        <span className="center-align col s3">
-                            <i className="material-icons image-icon" style={{ cursor: 'pointer' }}>delete</i>
-                        </span>
-                    </a>
-                </div>
-
-                :
-
-                <div className="card-content row">
-                    <span className="center-align col s4 ">
-                        < i onClick={() => upvote(imageId)}
-                            className={"material-icons image-icon upvote " + upvoted}>keyboard_arrow_up</i>
-                        {upvotes}
-                    </span>
-                    <Link to={'/s/' + imageId}>
-                        {/* avoid showing 0 views when you are the first viewer */}
-                        {share ?
-                            <span className="center-align col s4 ">
-                                <i className="material-icons image-icon">remove_red_eye</i> {views + 1}
-                            </span> :
-                            <span className="center-align col s4 ">
-                                <i className="material-icons image-icon">remove_red_eye</i> {views}
-                            </span>
-                        }
-
-                    </Link>
-                    <Link to={'/s/' + imageId}>
-                        <span className="center-align col s4 ">
-                            <i className="material-icons image-icon">comment</i> {numberOfComments}
-                        </span>
-                    </Link>
-                </div>}
-
-        </div >
-
-    )
+      {profile ? (
+        <div className="card-content row profile-image-stats">
+          <span className="center-align col s3 ">
+            {globalState.state.upvoted.includes(imageId) ? (
+              <i
+                onClick={(e) => upvote(imageId, e)}
+                className="material-icons image-icon upvoted upvote"
+              >
+                keyboard_arrow_up
+              </i>
+            ) : (
+              <i
+                onClick={() => upvote(imageId)}
+                className="material-icons image-icon upvote"
+              >
+                keyboard_arrow_up
+              </i>
+            )}
+            {upvotes}
+          </span>
+          <Link to={"/s/" + imageId}>
+            <span className="center-align col s3 ">
+              <i className="material-icons image-icon">remove_red_eye</i>{" "}
+              {views}
+            </span>
+          </Link>
+          <Link to={"/s/" + imageId}>
+            <span className="center-align col s3 ">
+              <i className="material-icons image-icon">comment</i>{" "}
+              {numberOfComments}
+            </span>
+          </Link>
+          <a
+            className="modal-trigger"
+            href="#deleteModal"
+            onClick={() => {
+              props.deleteImage(imageId);
+            }}
+          >
+            <span className="center-align col s3">
+              <i
+                className="material-icons image-icon"
+                style={{ cursor: "pointer" }}
+              >
+                delete
+              </i>
+            </span>
+          </a>
+        </div>
+      ) : (
+        <div className="card-content row">
+          <span className="center-align col s4 ">
+            <i
+              onClick={() => upvote(imageId)}
+              className={"material-icons image-icon upvote " + upvoted}
+            >
+              keyboard_arrow_up
+            </i>
+            {upvotes}
+          </span>
+          <Link to={"/s/" + imageId}>
+            {/* avoid showing 0 views when you are the first viewer */}
+            {share ? (
+              <span className="center-align col s4 ">
+                <i className="material-icons image-icon">remove_red_eye</i>{" "}
+                {views + 1}
+              </span>
+            ) : (
+              <span className="center-align col s4 ">
+                <i className="material-icons image-icon">remove_red_eye</i>{" "}
+                {views}
+              </span>
+            )}
+          </Link>
+          <Link to={"/s/" + imageId}>
+            <span className="center-align col s4 ">
+              <i className="material-icons image-icon">comment</i>{" "}
+              {numberOfComments}
+            </span>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/client/Components/Uploader.js
+++ src/client/Components/Uploader.js
@@ -1,93 +1,125 @@
-import React, { useState } from 'react'
-var QRCode = require('qrcode.react');
-
+import React, { useState } from "react";
+var QRCode = require("qrcode.react");
 
 // Import React FilePond
-import { FilePond, registerPlugin } from 'react-filepond';
+import { FilePond, registerPlugin } from "react-filepond";
 
 // Import FilePond styles
-import 'filepond/dist/filepond.min.css';
+import "filepond/dist/filepond.min.css";
 
 // Import the Image EXIF Orientation and Image Preview plugins
 // Note: These need to be installed separately
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 
 // Register the plugins
-registerPlugin(FilePondPluginFileValidateType, FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileValidateSize);
-
+registerPlugin(
+  FilePondPluginFileValidateType,
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateSize
+);
 
 function Uploader({ addImage }) {
-    const [file, setFile] = useState([]);
-    const [caption, setCaption] = useState('');
-    const [title, setTitle] = useState('');
-    const pond = React.createRef();
-    const upload = () => {
-        pond.current.processFiles();
-    }
+  const [file, setFile] = useState([]);
+  const [caption, setCaption] = useState("");
+  const [title, setTitle] = useState("");
+  const pond = React.createRef();
+  const [isUploading, setIsUploading] = useState(false);
+  const upload = () => {
+    setIsUploading(true);
+    pond.current.processFiles();
+  };
 
-    return (
-        <div>
-            {file.length !== 0 ?
-                <div className="row">
-                    <div className="input-field col s10">
-                        <textarea onChange={(e) => { setTitle(e.target.value) }} id="icon_prefix5" className="materialize-textarea"></textarea>
-                        <label htmlFor="icon_prefix5">Title</label>
-                    </div>
-                </div>
-                : null}
-            <FilePond
-                acceptedFileTypes={['image/png', 'image/jpeg', 'image/gif', 'video/mp4']}
-                ref={pond}
-                files={file}
-                allowMultiple={false}
-                onupdatefiles={(e) => setFile(e)}
-                server={{
-                    process: {
-                        ondata: (formData) => {
-                            formData.append('caption', caption);
-                            formData.append('title', title);
-                            return formData;
-                        },
-                        url: "/api/upload",
-                        onload: (res) => {
-                            res = JSON.parse(res)
-                            if (!res.errror) {
-                                addImage(res)
-                                setCaption('');
-                                setFile([]);
-                            }
-                        },
-
-                    },
-                    revert: null,
-                    restore: null,
-                    load: null,
-                    fetch: null,
-
-                }
-                }
-                allowProcess={false}
-                allowRevert={false}
-                instantUpload={false}
-                maxFileSize='5mb'
-            />
-            {file.length !== 0 ?
-                <div className="row">
-                    <div className="input-field col s10">
-                        <textarea onChange={(e) => { setCaption(e.target.value) }} id="icon_prefix4" className="materialize-textarea"></textarea>
-                        <label htmlFor="icon_prefix4">Caption</label>
-                    </div>
-                    <button className="btn" onClick={upload}><i className="material-icons prefix col s2">arrow_upward</i> Upload</button>
-
-                </div>
-                : null}
-
+  return (
+    <div>
+      {file.length !== 0 ? (
+        <div className="row">
+          <div className="input-field col s10">
+            <textarea
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              id="icon_prefix5"
+              className="materialize-textarea"
+            ></textarea>
+            <label htmlFor="icon_prefix5">Title</label>
+          </div>
         </div>
-    )
+      ) : null}
+      <FilePond
+        acceptedFileTypes={[
+          "image/png",
+          "image/jpeg",
+          "image/gif",
+          "video/mp4",
+        ]}
+        ref={pond}
+        files={file}
+        allowMultiple={false}
+        onupdatefiles={(e) => {
+          setFile(e);
+          setIsUploading(false);
+        }}
+        server={{
+          process: {
+            ondata: (formData) => {
+              formData.append("caption", caption);
+              formData.append("title", title);
+              return formData;
+            },
+            url: "/api/upload",
+            onload: (res) => {
+              res = JSON.parse(res);
+              if (!res.errror) {
+                addImage(res);
+                setCaption("");
+                setFile([]);
+              }
+            },
+          },
+          revert: null,
+          restore: null,
+          load: null,
+          fetch: null,
+        }}
+        allowProcess={false}
+        allowRevert={false}
+        instantUpload={false}
+        maxFileSize="5mb"
+      />
+      {file.length !== 0 ? (
+        <div className="row">
+          <div className="input-field col s10">
+            <textarea
+              onChange={(e) => {
+                setCaption(e.target.value);
+              }}
+              id="icon_prefix4"
+              className="materialize-textarea"
+            ></textarea>
+            <label htmlFor="icon_prefix4">Caption</label>
+          </div>
+          {isUploading ? (
+            <button className="btn">
+              <div class="spinner">
+                <div class="block"></div>
+                <div class="chain"></div>
+              </div>
+            </button>
+          ) : (
+            <button className="btn" onClick={upload}>
+              <i className="material-icons prefix col s2">arrow_upward</i>{" "}
+              Upload
+            </button>
+          )}
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default Uploader; 
+export default Uploader;
